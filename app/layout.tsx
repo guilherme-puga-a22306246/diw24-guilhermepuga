9de0b1ebdea9bbe7339144f8c1caf6119b5fb823
guilhermepuga-a22306246 <150610732+guilherme-puga-a22306246@users.noreply.github.com>
2024-12-11 17:58:38 +0000

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,11 +26,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="pt">
+      <body>
+        <Header />
+        <main>(children)</main>
+        <Footer />
       </body>
     </html>
   );
