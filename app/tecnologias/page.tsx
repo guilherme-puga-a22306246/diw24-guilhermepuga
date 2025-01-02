799241489b38f9ec7b958a52e515a2bac769c31b
guilhermepuga-a22306246 <150610732+guilherme-puga-a22306246@users.noreply.github.com>
2025-01-02 20:59:43 +0000

    lab 11 corrigido

--- app/tecnologias/page.tsx
+++ app/tecnologias/page.tsx
@@ -4,14 +4,31 @@ import TechCard from '../../components/Card/TechCard'
 import styles from './page.module.css'
 import tecnologias from '@/app/data/tecnologias.json'
 
+interface Rating {
+  rate: number
+  count: number
+}
+
+interface Technology {
+  id: number
+  title: string
+  image: string
+  description: string
+  rating: Rating
+}
+
+interface TecnologiasData {
+  tecnologias: Technology[]
+}
+
 export default function Tecnologias() {
-  const tecData = JSON.parse(JSON.stringify(tecnologias))
+  const tecData: TecnologiasData = JSON.parse(JSON.stringify(tecnologias))
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Tecnologias</h1>
       <div className={styles.grid}>
-        {tecData.tecnologias.map((tech) => (
+        {tecData.tecnologias.map((tech: Technology) => (
           <TechCard key={tech.id} tech={tech} />
         ))}
       </div>
